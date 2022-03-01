56e5991d2869d3f0da185b7366d7a2f825e6bfab
Angada28 <angad2011@hotmail.com>
2022-03-01 09:20:58 -0500

    files added

---
+++ Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Pb1 = new System.Windows.Forms.PictureBox();
+            this.Pb2 = new System.Windows.Forms.PictureBox();
+            this.Pb3 = new System.Windows.Forms.PictureBox();
+            this.Pb4 = new System.Windows.Forms.PictureBox();
+            this.Pb5 = new System.Windows.Forms.PictureBox();
+            this.Pb6 = new System.Windows.Forms.PictureBox();
+            this.Pb7 = new System.Windows.Forms.PictureBox();
+            this.Pb8 = new System.Windows.Forms.PictureBox();
+            this.Pb9 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Pb1
+            // 
+            this.Pb1.BackColor = System.Drawing.Color.Transparent;
+            this.Pb1.Image = ((System.Drawing.Image)(resources.GetObject("Pb1.Image")));
+            this.Pb1.Location = new System.Drawing.Point(12, 3);
+            this.Pb1.Name = "Pb1";
+            this.Pb1.Size = new System.Drawing.Size(130, 152);
+            this.Pb1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb1.TabIndex = 0;
+            this.Pb1.TabStop = false;
+            this.Pb1.Click += new System.EventHandler(this.clicked);
+            // 
+            // Pb2
+            // 
+            this.Pb2.BackColor = System.Drawing.Color.Transparent;
+            this.Pb2.Image = ((System.Drawing.Image)(resources.GetObject("Pb2.Image")));
+            this.Pb2.Location = new System.Drawing.Point(187, 3);
+            this.Pb2.Name = "Pb2";
+            this.Pb2.Size = new System.Drawing.Size(130, 152);
+            this.Pb2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb2.TabIndex = 1;
+            this.Pb2.TabStop = false;
+            this.Pb2.Click += new System.EventHandler(this.clicked);
+            // 
+            // Pb3
+            // 
+            this.Pb3.BackColor = System.Drawing.Color.Transparent;
+            this.Pb3.Image = ((System.Drawing.Image)(resources.GetObject("Pb3.Image")));
+            this.Pb3.Location = new System.Drawing.Point(351, 3);
+            this.Pb3.Name = "Pb3";
+            this.Pb3.Size = new System.Drawing.Size(130, 152);
+            this.Pb3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb3.TabIndex = 2;
+            this.Pb3.TabStop = false;
+            this.Pb3.Click += new System.EventHandler(this.clicked);
+            // 
+            // Pb4
+            // 
+            this.Pb4.BackColor = System.Drawing.Color.Transparent;
+            this.Pb4.Image = ((System.Drawing.Image)(resources.GetObject("Pb4.Image")));
+            this.Pb4.Location = new System.Drawing.Point(12, 170);
+            this.Pb4.Name = "Pb4";
+            this.Pb4.Size = new System.Drawing.Size(130, 152);
+            this.Pb4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb4.TabIndex = 3;
+            this.Pb4.TabStop = false;
+            this.Pb4.Click += new System.EventHandler(this.clicked);
+            // 
+            // Pb5
+            // 
+            this.Pb5.BackColor = System.Drawing.Color.Transparent;
+            this.Pb5.Image = ((System.Drawing.Image)(resources.GetObject("Pb5.Image")));
+            this.Pb5.Location = new System.Drawing.Point(187, 173);
+            this.Pb5.Name = "Pb5";
+            this.Pb5.Size = new System.Drawing.Size(130, 152);
+            this.Pb5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb5.TabIndex = 4;
+            this.Pb5.TabStop = false;
+            this.Pb5.Click += new System.EventHandler(this.clicked);
+            // 
+            // Pb6
+            // 
+            this.Pb6.BackColor = System.Drawing.Color.Transparent;
+            this.Pb6.Image = ((System.Drawing.Image)(resources.GetObject("Pb6.Image")));
+            this.Pb6.Location = new System.Drawing.Point(351, 173);
+            this.Pb6.Name = "Pb6";
+            this.Pb6.Size = new System.Drawing.Size(130, 152);
+            this.Pb6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb6.TabIndex = 5;
+            this.Pb6.TabStop = false;
+            this.Pb6.Click += new System.EventHandler(this.clicked);
+            // 
+            // Pb7
+            // 
+            this.Pb7.BackColor = System.Drawing.Color.Transparent;
+            this.Pb7.Image = ((System.Drawing.Image)(resources.GetObject("Pb7.Image")));
+            this.Pb7.Location = new System.Drawing.Point(12, 349);
+            this.Pb7.Name = "Pb7";
+            this.Pb7.Size = new System.Drawing.Size(130, 152);
+            this.Pb7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb7.TabIndex = 6;
+            this.Pb7.TabStop = false;
+            this.Pb7.Click += new System.EventHandler(this.clicked);
+            // 
+            // Pb8
+            // 
+            this.Pb8.BackColor = System.Drawing.Color.Transparent;
+            this.Pb8.Image = ((System.Drawing.Image)(resources.GetObject("Pb8.Image")));
+            this.Pb8.Location = new System.Drawing.Point(187, 340);
+            this.Pb8.Name = "Pb8";
+            this.Pb8.Size = new System.Drawing.Size(130, 152);
+            this.Pb8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb8.TabIndex = 7;
+            this.Pb8.TabStop = false;
+            this.Pb8.Click += new System.EventHandler(this.clicked);
+            // 
+            // Pb9
+            // 
+            this.Pb9.BackColor = System.Drawing.Color.Transparent;
+            this.Pb9.Image = ((System.Drawing.Image)(resources.GetObject("Pb9.Image")));
+            this.Pb9.Location = new System.Drawing.Point(351, 340);
+            this.Pb9.Name = "Pb9";
+            this.Pb9.Size = new System.Drawing.Size(130, 152);
+            this.Pb9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pb9.TabIndex = 8;
+            this.Pb9.TabStop = false;
+            this.Pb9.Click += new System.EventHandler(this.clicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Tic_Tac_Toe.Properties.Resources.tic_tac_toe;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(511, 493);
+            this.Controls.Add(this.Pb9);
+            this.Controls.Add(this.Pb8);
+            this.Controls.Add(this.Pb7);
+            this.Controls.Add(this.Pb6);
+            this.Controls.Add(this.Pb5);
+            this.Controls.Add(this.Pb4);
+            this.Controls.Add(this.Pb3);
+            this.Controls.Add(this.Pb2);
+            this.Controls.Add(this.Pb1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb9)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Pb1;
+        private System.Windows.Forms.PictureBox Pb2;
+        private System.Windows.Forms.PictureBox Pb3;
+        private System.Windows.Forms.PictureBox Pb4;
+        private System.Windows.Forms.PictureBox Pb5;
+        private System.Windows.Forms.PictureBox Pb6;
+        private System.Windows.Forms.PictureBox Pb7;
+        private System.Windows.Forms.PictureBox Pb8;
+        private System.Windows.Forms.PictureBox Pb9;
+    }
+}
+
---
+++ Tic Tac Toe/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        #region variables
+        //gets filled with either x or o based on the picture box that is clicked 
+        string[] checkboard = { "", "", "", "", "", "", "", "", "" };
+        //used to help determine who goes next
+        bool currentTurn = true;
+        //used to help check for a draw
+        int ctr = 0;
+        Random box = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            move1();
+        }
+        #endregion
+        //decides who goes first
+        private void move1()
+        {
+            switch (box.Next(2))
+            {
+                case 0:
+                    currentTurn = true;
+                    MessageBox.Show("X will go first");
+                    break;
+                case 1:
+                    currentTurn = false;
+                    MessageBox.Show("O will go first");
+                    break;
+            }
+        }
+        //makes the image appear, decides who goes next and fill spot in the array
+        private void clicked(object sender, EventArgs e)
+        {
+            PictureBox picked = (PictureBox)sender;
+            char[] brokenname = picked.Name.ToCharArray();
+            string fullnum = brokenname[2].ToString();
+            int numinname = int.Parse(fullnum) - 1;
+            switch (currentTurn)
+            {
+                case true:
+                    picked.Image = Properties.Resources.tic_tac_toe_X;
+                    picked.Enabled = false;
+                    ctr++;                    
+                    checkboard[numinname] = "x";
+                    currentTurn = false;
+                    break;
+                case false:
+                    picked.Image = Properties.Resources.letter_o_icon_2_256;
+                    picked.Enabled = true;
+                    ctr++;
+                    checkboard[numinname] = "o";
+                    currentTurn = true;
+                    break;
+            }
+            WinCheck();
+        }
+        //checks for all 17 outcomes
+        private void WinCheck()
+        {
+            // if x matches horizontal
+            if (checkboard[0] == checkboard[1] && checkboard[1] == checkboard[2] && checkboard[2] == "x")
+            {
+                messagex();
+            }
+            else if (checkboard[3] == checkboard[4] && checkboard[4] == checkboard[5] && checkboard[5] == "x")
+            {
+                messagex();
+            }
+            else if (checkboard[6] == checkboard[7] && checkboard[7] == checkboard[8] && checkboard[8] == "x")
+            {
+                messagex();
+            }
+            //if x wins vertical
+            else if (checkboard[0] == checkboard[3] && checkboard[3] == checkboard[6] && checkboard[6] == "x")
+            {
+                messagex();
+            }
+            else if (checkboard[1] == checkboard[4] && checkboard[4] == checkboard[7] && checkboard[7] == "x")
+            {
+                messagex();
+            }
+            else if (checkboard[2] == checkboard[5] && checkboard[5] == checkboard[8] && checkboard[8] == "x")
+            {
+                messagex();
+            }
+            //if x wins diagonal
+            else if (checkboard[0] == checkboard[4] && checkboard[4] == checkboard[8] && checkboard[8] == "x")
+            {
+                messagex();
+            }
+            else if (checkboard[6] == checkboard[4] && checkboard[4] == checkboard[2] && checkboard[2] == "x")
+            {
+                messagex();
+            }           
+            // if o matches horizontal
+            if (checkboard[0] == checkboard[1] && checkboard[1] == checkboard[2] && checkboard[2] == "o")
+            {
+                messageo();
+            }
+            else if (checkboard[3] == checkboard[4] && checkboard[4] == checkboard[5] && checkboard[5] == "o")
+            {
+                messageo();
+            }
+            else if (checkboard[6] == checkboard[7] && checkboard[7] == checkboard[8] && checkboard[8] == "o")
+            {
+                messageo();
+            }
+            //if o wins vertical
+            else if (checkboard[0] == checkboard[3] && checkboard[3] == checkboard[6] && checkboard[6] == "o")
+            {
+                messageo();
+            }
+            else if (checkboard[1] == checkboard[4] && checkboard[4] == checkboard[7] && checkboard[7] == "o")
+            {
+                messageo();
+            }
+            else if (checkboard[2] == checkboard[5] && checkboard[5] == checkboard[8] && checkboard[8] == "o")
+            {
+                messageo();
+            }
+            //if o wins diagonal
+            else if (checkboard[0] == checkboard[4] && checkboard[4] == checkboard[8] && checkboard[8] == "o")
+            {
+                messageo();
+            }
+            else if (checkboard[6] == checkboard[4] && checkboard[4] == checkboard[2] && checkboard[2] == "o")
+            {
+                messageo();
+            }
+            //no one wins
+            else if (ctr == 9)
+            {
+                MessageBox.Show("no one wins"); if (MessageBox.Show("Do you want to try again?", "Continue?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    Application.Restart();
+                }
+                else
+                {
+                    Application.Exit();
+                }
+            }
+        }
+        private void messagex()
+        {
+            MessageBox.Show("x wins"); if (MessageBox.Show("Do you want to try again?", "Continue?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Restart();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+        private void messageo()
+        {
+            MessageBox.Show("O wins"); if (MessageBox.Show("Do you want to try again?", "Continue?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Restart();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
